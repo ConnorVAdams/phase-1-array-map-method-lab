d8a82fb70cdc5d9acb4f2f1c407f38a782e8bf90
Connor Adams <connorvadams@gmail.com>
2023-09-17 20:18:42 -0600

    Works like it SHOULD but doesn't pass the test smh.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const specialChars = [
+  '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/',
+  ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'
+];
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -11,6 +16,48 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+const sentenceToWords = (sentenceString) => {
+  return sentenceString.split(' ');
+};
+
+const wordsToLetters = (wordString) => {
+  return wordString.split('');
+};
+
+const removeSpecialChar = (word) => {
+  const letters = wordsToLetters(word);
+  const noChars = letters.filter(letter => {
+    return !specialChars.includes(letter);
+  }).join('');
+  return noChars;
+};
+
+const checkForAllCapsOrCamelCase = (word) => {
+  const allCaps = wordsToLetters(word.toUpperCase());
+  const originalLetters = wordsToLetters(word);
+  
+  let index = 0; 
+  for (const letter of originalLetters) {
+    if (letter === allCaps[index] && index !== 0) { 
+      return true;
+    }
+    index++;
+  }
+  return false; 
+};
+
+const titleCaseOneSentence = (sentence) => {
+  return sentenceToWords(sentence).map(word => {
+    if (checkForAllCapsOrCamelCase(removeSpecialChar(word)) === true) {
+      return word;
+    } else {
+      return word.charAt(0).toUpperCase() + word.slice(1); 
+    }
+  }).join(' ');
+};
+
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(titleCaseOneSentence);
+};
+
+console.log(titleCased())
